6b8c1c9666693cbe6e5c1adbf2be36f2f433b415
victorpss <104112503+victorpss@users.noreply.github.com>
2022-06-09 22:32:45 -0300

    Update and rename README.md to Introduction.c

---
+++ Introduction.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+void main (void){
+	printf("Hello world!\n");
+	printf("My name is Victor Santos and I'm 18 years old.\nI'm currently a Computer Science student at UFRJ (Federal University of Rio de Janeiro).\nAs a beginner, I'm completely open to suggestions and advices.\nYou can reach me on Discord (Victør#4319) and I will answer you as soon as possible.")
+	}
--- README.md
+++
@@ -1,5 +0,0 @@
-	Hello world!
-My name is Victor Santos and I'm 18 years old.
-I'm currently a Computer Science student at UFRJ (Federal University of Rio de Janeiro).
-As a beginner, I'm completely open to suggestions and advices.
-You can reach me on Discord (Victør#4319). I will answer you as soon as possible.
